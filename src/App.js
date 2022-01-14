50c5a5848360c8b2dd4ad7a9a0e60703f0eea9a5
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-01-15 00:07:53 +0700

    feat: routing pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Employee, Register } from './pages';
+import { CompanyHome, Customer, Employee, Home, Register, Scrum, ScrumDetail, Team } from './pages';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/employee' element={<Employee />}/>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/company/dashboard' element={<CompanyHome />}/>
+            <Route path='/company/employee' element={<Employee />}/>
+            <Route path='/company/customer' element={<Customer />}/>
+            <Route path='/company/team' element={<Team />}/>
+            <Route path='/company/scrum' element={<Scrum />}/>
+            <Route path='/company/scrum/detail/:id' element={<ScrumDetail />}/>
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
