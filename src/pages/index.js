82ce13b59137bb820c33b146a5165eca90f00365
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-01-15 00:25:07 +0700

    feat: function hooks to call pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,8 @@
 export {default as Register} from './Register'
 export {default as Employee} from './Employee'
+export {default as Home} from './Home'
+export {default as CompanyHome} from './CompanyHome'
+export {default as Customer} from './Customer'
+export {default as Scrum} from './Scrum'
+export {default as Team} from './Team'
+export {default as ScrumDetail} from './ScrumDetail'
